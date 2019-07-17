0ab0efe53809d6e12c72bf52952f9f8d86237db6
Mary Kate <mcaucci@fordham.edu>
2019-07-17 15:19:35 -0400

    some modifications and additions

--- Methane-VibFreqAnalysis.py
+++ Methane-VibFreqAnalysis.py
@@ -24,25 +24,35 @@ psi4.set_options({'g_convergence': 'gau_tight'})
 psi4.optimize('scf/cc-pVDZ', molecule=ch4)
 
 
+
 # Run vibrational frequency analysis
 psi4.set_output_file(file_prefix + '_vibfreq.dat', False)
 scf_energy, scf_wfn = psi4.frequency('scf/cc-pVDZ', molecule=ch4, return_wfn=True, dertype='gradient')
 
+
 # Save "raw" frequencies into a variable
 print(scf_wfn.frequency_analysis) # this command is just to get you started!
+freqlist = scf_wfn.frequency_analysis['omega'][2]
 
 # Eliminate imaginary parts of frequencies,
+realfreqlist = np.real(freqlist)
 # round the frequencies (to the nearest whole number),
+roundfreqlist = np.round(realfreqlist)
 # and extract only the *non-zero* frequencies
-
+nonzerofreqlist = roundfreqlist[6:]
 
 # Determine the unique non-zero frequencies and 
+freqtable=np.array(np.unique(nonzerofreqlist, return_counts = True))
 # the number of times each such frequency occurs;
+
 # store these in a NumPy array in the format: 
+freqtable.T
+print(freqtable.T)
 # {frequency, count} (i.e, one line per freq.)
 
 
 # Save the NumPy array with frequency and count data
 # to a text file
+np.savetxt(fname='MethaneList.dat', X=freqtable.T, fmt='%d %d', header='Freq Degen')
 
 
