9b270c0103c65db667b59251630b675134938bab
Xinqiao Zhang <xzhang304@fordham.edu>
2019-07-17 15:11:02 -0400

    new stuff

--- Methane-VibFreqAnalysis.py
+++ Methane-VibFreqAnalysis.py
@@ -29,20 +29,29 @@ psi4.set_output_file(file_prefix + '_vibfreq.dat', False)
 scf_energy, scf_wfn = psi4.frequency('scf/cc-pVDZ', molecule=ch4, return_wfn=True, dertype='gradient')
 
 # Save "raw" frequencies into a variable
-print(scf_wfn.frequency_analysis) # this command is just to get you started!
+freq = scf_wfn.frequency_analysis['omega'][2]
+freqLen = len(freq)
 
 # Eliminate imaginary parts of frequencies,
+freqReal = np.empty((freqLen))
+for i in range(freqLen):
+    freqReal[i] = freq[i].real
+    
 # round the frequencies (to the nearest whole number),
+freqRound = np.around(freqReal)
 # and extract only the *non-zero* frequencies
-
+freqNonZero = freqRound[6:]
 
 # Determine the unique non-zero frequencies and 
 # the number of times each such frequency occurs;
+unique, counts = np.unique(freqNonZero, return_counts=True)
 # store these in a NumPy array in the format: 
 # {frequency, count} (i.e, one line per freq.)
-
+freqAndDegen = np.empty((3,2))
+for i in range(3):
+    freqAndDegen[i] = [unique[i], counts[i]]
 
 # Save the NumPy array with frequency and count data
 # to a text file
-
+np.savetxt('methane_freq_and_degen.txt',  freqAndDegen, fmt = '%d', header = 'Frequencies and degeneracies')
 
