6015b29c52ea872c011e1e08d09923636ce46499
Logan Smith <19smithlogan@gmail.com>
2022-07-20 13:44:29 -0400

    Editted Methane vibrational frequency script and added text file with vibrational frequencies

--- Methane-VibFreqAnalysis.py
+++ Methane-VibFreqAnalysis.py
@@ -29,20 +29,33 @@ psi4.set_output_file(file_prefix + '_vibfreq.dat', False)
 scf_energy, scf_wfn = psi4.frequency('scf/cc-pVDZ', molecule=ch4, return_wfn=True, dertype='gradient')
 
 # Save "raw" frequencies into a variable
-print(scf_wfn.frequency_analysis) # this command is just to get you started!
-
+raw = np.array(scf_wfn.frequency_analysis['omega'].data) # this command is just to get you started!
+print('numpy array')
+#print(raw)
 # Eliminate imaginary parts of frequencies,
+real = np.real(raw)
+print('remove imag')
+print(real)
+print()
 # round the frequencies (to the nearest whole number),
+rounded = np.round_(real,0)
+print('rounded')
+print(rounded)
+print()
 # and extract only the *non-zero* frequencies
+nonzeros = np.trim_zeros(rounded)
+print('nonzeros')
+print(nonzeros)
+print()
 
-
-# Determine the unique non-zero frequencies and 
+# Determine the unique non-zero frequencies and
 # the number of times each such frequency occurs;
-# store these in a NumPy array in the format: 
+# store these in a NumPy array in the format:
 # {frequency, count} (i.e, one line per freq.)
-
-
+unique = np.unique(nonzeros, return_counts=True)
+print(unique[1])
+stacked = np.column_stack((unique[0],unique[1]))
+print(stacked)
 # Save the NumPy array with frequency and count data
 # to a text file with the header line: 'freq degen'
-
-
+np.savetxt('Methane_Frequencies.txt',stacked,header='Methane Frequency Fun')
